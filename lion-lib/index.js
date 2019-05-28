7761d6f77a89065dc3bce17a0c401f536b7cb1c7
niesssiobhan <niesssiobhan@live.com>
2019-05-28 16:25:02 -0700

    done with a section

---
+++ lion-lib/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports.add = function(a, b) {return a + b};
+
+module.exports.multiple = function(a, b) {return a * b}; // this is a new feature that has been added to the npm package
---
+++ node-app/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+let lion = require('lion-lib-niess');
+let result = lion.add(1, 2);
+console.log(result);
