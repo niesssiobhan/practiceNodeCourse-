80973f95e3addd3c61c318a71c5880180c4b380d
niesssiobhan <niesssiobhan@live.com>
2019-05-30 12:42:50 -0700

    moving on to playgorund practice with promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -3,6 +3,7 @@
 // When you run node index.js in the terminal the result will be ""Before" then "After" and then the last message will be "reading a uses from a database"
 
 console.log('before');
+// this nested structure is due to the fact that callbacks were being used 
 getUser(1, (user) => {
   getRepositories(user.gitHubUsername, (repos) => {
     getCommits(repos[0], (commits) => {
@@ -11,8 +12,12 @@ getUser(1, (user) => {
   })
 });
 
-const p = getUser(1);
-p.then
+// this code belowis the same as the code from above but promises are now being used so it results in clean looking code 
+getUser(1)
+  .then(user => getRepositories(user.gitHubUsername))
+  .then(repos => getCommits(repos[0]))
+  .then(commits => console.log('commits', commits))
+  .catch(err => console.log('Error', err.message));
 
 console.log('after');
 
@@ -23,7 +28,7 @@ console.log('after');
 // const commits = getCommits(repos[0]);
 // console.log('After');
 
-// the code below has been refactored from an asynchronous callback hell into simple code (code is shown above that was the before)
+// the code below has been refactored from an asynchronous callback hell into simple code (code is shown above that was the before and the result are the 3 functions below)
 console.log('Before');
 getUser(1, getRepositories);
 console.log('After');
@@ -49,6 +54,7 @@ function displayCommits(commits) {
 
 // this is an example oof an asynchronous or non-blocking function
 // how this is going to work is that after 2 seconds (2000) then the function within the setTimeout will run
+// the function below are done with promises and with cleaner looking code 
 function getUser(id) {
   return new Promise ((resolve, reject) => {
     setTimeout(() => {
