d859b1894104af62b3da5e06aaa90c4dacdd0e41
niesssiobhan <niesssiobhan@live.com>
2019-05-30 09:18:35 -0700

    working on project part of course

--- vidly/index.js
+++ vidly/index.js
@@ -1,13 +1,14 @@
 'use strict';
 
+// these 3 lines below is how you build a web server
 const express = require('express');
 const Joi = require('joi');
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // this returns a middleware function
 
 app.get('/', (req, res) => {
-
+  res.send('It is working!')
 });
 
 app.get('/api/genres/:id', (req, res) => {
@@ -15,7 +16,16 @@ app.get('/api/genres/:id', (req, res) => {
 });
 
 app.post('/api/genres', (req, res) => {
+  const {error} = validateCourse(req.body); 
+  if(error) return res.status(400).send(result.error.details[0].message);
 
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+
+  courses.push(course); // this is pushing it into the courses array
+  res.send(course); // this is returning the object in the body of the response 
 });
 
 app.put('/api/genres/:id', (req, res) => {
@@ -28,7 +38,7 @@ app.delete('/api/genres/:id', (req, res) => {
 
 function validateGenre(genre) {
   const schema = {
-    name = Joi.string().min(3).require()
+    name = Joi.string().min(3).required() 
   };
   return validate (genre, schema);
 }
