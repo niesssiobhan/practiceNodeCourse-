ea7c06ad0620929af9177d02055adc55b0cccc15
niesssiobhan <niesssiobhan@live.com>
2019-06-03 11:56:04 -0700

    more dependencies

---
+++ vidly/startup/prod.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const helmet = require('helmet');
+const compression = require('compression');
+
+module.exports = function(app) {
+  app.use(helmet());
+  app.use(compression());
+}
