0301ea687d20e260d85f32b0a034b6a9116f2e22
niesssiobhan <niesssiobhan@live.com>
2019-06-03 08:10:33 -0700

    fixed

---
+++ vidly/middleware/validateObjectId.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+module.exports = function (req, res, next) {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id))
+    return res.status(404).send('invalid id');
+
+  next();
+}
